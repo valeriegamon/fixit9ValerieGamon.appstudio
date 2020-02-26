d90bba09bdbe6371c03fa34c8467862fb4040d03
valeriegamon <valeriegamon@valeries-mbp.ddns.creighton.edu>
2020-02-26 10:22:32 -0600

    Update membership.js

--- forms/membership/membership.js
+++ forms/membership/membership.js
@@ -4,10 +4,51 @@ let members = ["Mary","Sam","Joey","Paul","Mike","Char","Jenny","Kennedy","Kaden
 
 submitnames.onclick=function(){
   memberName = inptFirstName.value
-  if ((memberName == members[0]) || (memberName == members[1]) || (memberName == members[2]) || (memberName == members[3]) || (memberName == members[4]) || (memberName == members[5]) || (memberName == members[6]) || (memberName == members[7]) || (memberName == members[8]) || (memberName == members[9]) || (memberName == members[10])) {
+  if (memberName == members[0]) {
     name.push(inptFirstName.value)
     NSB.MsgBox("Congrats! You are a member!")
-  } else  {
+    //alert(name)
+   } else  if (memberName == members[1]) {
+    name.push(inptFirstName.value)
+    NSB.MsgBox("Congrats! You are a member!")
+    //alert(name)
+    } else  if (memberName == members[2]) {
+    name.push(inptFirstName.value)
+    NSB.MsgBox("Congrats! You are a member!")
+    //alert(name)
+    } else  if (memberName == members[3]) {
+    name.push(inptFirstName.value)
+    NSB.MsgBox("Congrats! You are a member!")
+    //alert(name)
+    } else  if (memberName == members[4]) {
+    name.push(inptFirstName.value)
+    NSB.MsgBox("Congrats! You are a member!")
+    //alert(name)
+    } else  if (memberName == members[5]) {
+    name.push(inptFirstName.value)
+    NSB.MsgBox("Congrats! You are a member!")
+    //alert(name)
+    } else  if (memberName == members[6]) {
+    name.push(inptFirstName.value)
+    NSB.MsgBox("Congrats! You are a member!")
+    //alert(name)
+    } else  if (memberName == members[7]) {
+    name.push(inptFirstName.value)
+    NSB.MsgBox("Congrats! You are a member!")
+    //alert(name)
+    } else  if (memberName == members[8]) {
+    name.push(inptFirstName.value)
+    NSB.MsgBox("Congrats! You are a member!")
+    //alert(name)
+    } else  if (memberName == members[9]) {
+    name.push(inptFirstName.value)
+    NSB.MsgBox("Congrats! You are a member!")
+    //alert(name)
+  } else if (memberName == members[10]) {
+    name.push(inptFirstName.value)
+    NSB.MsgBox("Congrats! You are a member!")
+    //alert(name)
+    } else {
     NSB.MsgBox("You are not a member. Please try again.");
     inptFirstName.value = ""
     }
