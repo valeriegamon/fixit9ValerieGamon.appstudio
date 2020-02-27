aad2d022aedc5615a65e717e2e5ff0dece3b0d29
valeriegamon <valeriegamon@valeries-mbp.ddns.creighton.edu>
2020-02-27 10:09:32 -0600

    nn

--- forms/login/login.js
+++ forms/login/login.js
@@ -5,7 +5,7 @@ let accountName = ""
 btnSubmit.onclick=function(){
   accountName = inptAccount.value
   let pass = inptPassword.value
-  if ((accountName == "Smith") && (pass == "123Dogs#"))
+  if ((accountName == "Smith") && (pass == "123Dogs#")) 
     ChangeForm(ABMadLibs)
     //I made this change to madlibs to make it easier to reference the name of the user
     //if you  want to go to membership form, do this instead: ChangeForm(membership)
