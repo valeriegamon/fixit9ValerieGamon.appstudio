a26304316b77317b56935bbdb5fd6d5974f63f0c
valeriegamon <valeriegamon@valeries-mbp.ddns.creighton.edu>
2020-02-27 09:56:35 -0600

    Update login.js

--- forms/login/login.js
+++ forms/login/login.js
@@ -8,6 +8,7 @@ btnSubmit.onclick=function(){
   if ((accountName == "Smith") && (pass == "123Dogs#"))
     ChangeForm(ABMadLibs)
     //I made this change to madlibs to make it easier to reference the name of the user
+    //if you  want to go to membership form, do this instead: ChangeForm(membership)
   else    
     NSB.MsgBox("That login is not recognised. Please try again.")
     inptAccount.value = ""
